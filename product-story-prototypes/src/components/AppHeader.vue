cac0e9495f50ca155dbf52c9411b4f72191446f5
Christopher Bartling <chris.bartling@gmail.com>
2019-01-04 21:02:48 -0600

    Initial revision.

---
+++ product-story-prototypes/src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app-header">
+    <div class="brand">
+      <div class="brand-logo">
+        <img
+          alt="ProductStory logo"
+          src="@/assets/logo.png"
+          height="50"
+          width="50"
+        />
+      </div>
+      <div class="brand-name">ProductStory</div>
+    </div>
+    <UserProfileImage></UserProfileImage>
+  </div>
+</template>
+
+<script>
+import UserProfileImage from "./UserProfileImage";
+export default {
+  name: "AppHeader",
+  components: { UserProfileImage }
+};
+</script>
+
+<style scoped lang="scss">
+.app-header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  background-color: #00005f;
+  color: #ffffff;
+  padding: 8px;
+
+  .brand {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: center;
+
+    .brand-logo {
+    }
+
+    .brand-name {
+      font-family: Roboto, sans-serif;
+      font-size: 2rem;
+      font-weight: bold;
+    }
+  }
+}
+</style>
---
+++ product-story-prototypes/src/components/UserProfileImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="user-profile-picture-wrapper">
+    <img src="@/assets/user-profile.png" class="image-cover" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfileImage"
+};
+</script>
+
+<style scoped lang="scss">
+.user-profile-picture-wrapper {
+  padding: 10px;
+
+  .image-cover {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin: 4px;
+
+    object-fit: cover;
+    object-position: center right;
+  }
+}
+</style>
