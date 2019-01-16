a7e4c8fe323c165b613c58346b5a42a24f83ce68
Christopher Bartling <chris.bartling@gmail.com>
2019-01-16 00:44:18 -0600

    Adding dropdown for user story maps.

---
+++ product-story-prototypes/src/components/userStoryMap/UserStoryMapPicker.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-xs-left">
+    <v-layout wrap>
+      <v-flex xs12>
+        <v-combobox
+          v-model="selectedMap"
+          :items="availableUserStoryMaps"
+          label="Select an existing user story map to work with"
+        ></v-combobox>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserStoryMapPicker",
+  data() {
+    return {
+      selectedMap: "Programming",
+      availableUserStoryMaps: ["Programming", "Design", "Vue", "Vuetify"]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- product-story-prototypes/src/views/UserStoryMap.vue
+++ product-story-prototypes/src/views/UserStoryMap.vue
@@ -5,6 +5,7 @@
         <v-flex xs4>
           <CreateUserStoryMapDialog></CreateUserStoryMapDialog>
         </v-flex>
+        <v-flex xs8> <UserStoryMapPicker></UserStoryMapPicker> </v-flex>
       </v-layout>
       <v-layout row wrap>
         <!--<v-flex xs12> <CreateActivityDialog></CreateActivityDialog> </v-flex>-->
@@ -16,10 +17,12 @@
 <script>
 // import CreateActivityDialog from "@/components/userStoryMap/CreateActivityDialog";
 import CreateUserStoryMapDialog from "../components/userStoryMap/CreateUserStoryMapDialog";
+import UserStoryMapPicker from "../components/userStoryMap/UserStoryMapPicker";
 
 export default {
   name: "UserStoryMap",
   components: {
+    UserStoryMapPicker,
     CreateUserStoryMapDialog
   },
   methods: {}
