643d426ee29b861cc06adb7f787d2e3ff293b060
Christopher Bartling <chris.bartling@gmail.com>
2019-01-28 23:31:46 -0600

    Using domain object with observability.

--- product-story-prototypes/src/domain/UserStoryMap.js
+++ product-story-prototypes/src/domain/UserStoryMap.js
@@ -6,6 +6,7 @@ class UserStoryMap {
     this.id = id;
     this.updateAttributes(data);
     this.startObserving();
+    this.startObservingActivitiesCollection();
   }
 
   updateAttributes(data) {
@@ -30,9 +31,32 @@ class UserStoryMap {
     }
   }
 
+  startObservingActivitiesCollection() {
+    try {
+      const { uid } = store.state.common.user;
+      const collectionPath = `users/${uid}/userStoryMaps/${this.id}/activities`;
+      this.activitiesCollection = firestore().collection(collectionPath);
+      this.unsubscribeActivitiesFunction = this.activitiesCollection.onSnapshot(
+        querySnapshot => {
+          // const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+          // this.updateAttributes(doc.data());
+          console.info("Activities collection changed.", querySnapshot);
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   stopObserving() {
     if (this.unsubscribeFunction) {
       this.unsubscribeFunction();
+    }
+  }
+
+  stopObservingActivitiesCollection() {
+    if (this.unsubscribeActivitiesFunction) {
+      this.unsubscribeActivitiesFunction();
     }
   }
 }
