3247a736e1d394103521644647a0d167ee6fe722
Christopher Bartling <chris.bartling@gmail.com>
2019-02-05 23:53:02 -0600

    Added the user story editor dialog.

--- product-story-prototypes/src/components/userStoryMap/UserStoriesGrid.vue
+++ product-story-prototypes/src/components/userStoryMap/UserStoriesGrid.vue
@@ -1,26 +1,54 @@
 <template>
   <v-layout column pa-3>
-    <v-layout row v-for="row in activity.rows" :key="row">
+    <v-layout
+      row
+      v-for="row in activity.rows"
+      :key="row"
+      class="user-story-grid-row"
+    >
       <v-flex
         v-for="column in activity.columns"
         :key="column"
-        xs2
+        xs6
         class="user-story-grid-cell"
+        @click="dialogState.editorDialog = true"
       >
       </v-flex>
+    </v-layout>
+    <v-layout>
+      <UserStoryEditorDialog
+        :dialog="dialogState.editorDialog"
+        v-bind.sync="dialogState"
+      ></UserStoryEditorDialog>
     </v-layout>
   </v-layout>
 </template>
 
 <script>
+import UserStoryEditorDialog from "@/components/userStoryMap/UserStoryEditorDialog";
+
 export default {
   name: "UserStoriesGrid",
-  props: ["activity"]
+  components: {
+    UserStoryEditorDialog
+  },
+  props: ["activity"],
+  data() {
+    return {
+      dialogState: {
+        editorDialog: false
+      }
+    };
+  }
 };
 </script>
 
 <style scoped lang="scss">
+.user-story-grid-row {
+}
+
 .user-story-grid-cell {
+  margin: 5px;
   border-radius: 5px;
   border: #acd3d3 0.8px dashed;
   height: 70px;
---
+++ product-story-prototypes/src/components/userStoryMap/UserStoryEditorDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-dialog v-model="dialog" width="500">
+    <v-card>
+      <v-card-title class="headline grey lighten-2" primary-title>
+        Create User Story
+      </v-card-title>
+
+      <v-card-text>
+        <v-form v-model="valid">
+          <v-container>
+            <v-layout>
+              <v-flex xs12 md12>
+                <v-text-field
+                  v-model="title"
+                  label="Title"
+                  required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" flat @click="onClickCancel"> Cancel </v-btn>
+        <v-btn color="primary" flat @click="onClickSave"> Save </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "UserStoryEditorDialog",
+  props: {
+    dialog: Boolean
+  },
+  data() {
+    return {
+      valid: false,
+      title: undefined
+    };
+  },
+  methods: {
+    onClickCancel: function() {
+      this.$emit("update:editorDialog", false);
+    },
+    onClickSave: function() {
+      this.$emit("update:editorDialog", false);
+      // this.$store.dispatch("userStoryMap/createUserStory", {
+      //   title: this.title
+      // });
+    }
+  }
+};
+</script>
+
+<style scoped></style>
