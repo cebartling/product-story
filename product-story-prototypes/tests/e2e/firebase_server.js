b9bcc5dc2613bdfde76af0c29eaede478418504d
Christopher Bartling <chris.bartling@gmail.com>
2019-02-21 00:11:12 -0600

    Building up Firebase Admin server for use with Cypress.

--- product-story-prototypes/tests/e2e/firebase_server.js
+++ product-story-prototypes/tests/e2e/firebase_server.js
@@ -1,41 +1,44 @@
-const url = require("url");
+// const url = require("url");
 const minimist = require("minimist");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const session = require("express-session");
+// const session = require("express-session");
 const express = require("express");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://product-story-prototypes.firebaseio.com"
+});
 
 const app = express();
 
 // get port from passed in args from scripts/start.js
 const port = minimist(process.argv.slice(2)).port;
 
-const matchesUsernameAndPassword = (body = {}) => {
-  return body.username === "jane.lane" && body.password === "password123";
-};
+// const matchesUsernameAndPassword = (body = {}) => {
+//   return body.username === "jane.lane" && body.password === "password123";
+// };
 
 // parse regular form submission bodies
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(morgan("dev"));
 
-app.post("/login", urlencodedParser, (req, res) => {
-  // if this matches the secret username and password
-  if (matchesUsernameAndPassword(req.body)) {
-    // assume we always have this query param property
-    const redirectTo = req.query.redirectTo;
-
-    // parse the redirectTo invalid a valid URL object
-    const outgoing = url.parse(redirectTo);
-
-    // and add these query params to it with an arbitrary
-    // id_token (for a simple example)
-    outgoing.query = { id_token: "abc123def456" };
-
-    res.redirect(outgoing.format());
-  } else {
-    res.sendStatus(401);
-  }
+app.get("/helloWorld", urlencodedParser, (req, res) => {
+  admin
+    .auth()
+    .getUserByEmail("chris.bartling@gmail.com")
+    .then(userRecord => {
+      console.log(
+        `Found the user record: ${userRecord.uid}, email address: ${
+          userRecord.email
+        }`
+      );
+    });
+  res.send("Hello world, from the Express server!");
 });
 
 app.listen(port, () => {});
--- product-story-prototypes/tests/e2e/specs/views/About/dom.js
+++ product-story-prototypes/tests/e2e/specs/views/About/dom.js
@@ -1,6 +1,42 @@
+const _ = Cypress._;
+
+// require node's url module
+// const url = require("url");
+
 describe("About", () => {
+  Cypress.Commands.add("helloWorld", (overrides = {}) => {
+    Cypress.log({
+      name: "helloWorld"
+    });
+
+    const options = {
+      method: "GET",
+      url: "http://localhost:7075/helloWorld"
+      // qs: {
+      //   // use qs to set query string to the url that creates
+      //   // http://auth.corp.com:8080?redirectTo=http://localhost:7074/set_token
+      //   redirectTo: "http://localhost:7074/set_token"
+      // },
+      // form: true, // we are submitting a regular form body
+      // body: {
+      //   username: "jane.lane",
+      //   password: "password123"
+      // }
+    };
+
+    // allow us to override defaults with passed in overrides
+    _.extend(options, overrides);
+
+    cy.request(options);
+  });
+
   context("Visits the app root url and signs in", () => {
     beforeEach(() => {
+      cy.helloWorld().then(resp => {
+        cy.log("Hello world response", resp.body);
+        expect(resp.status).to.eq(200);
+        // expect(resp.json).to.include("<h1>Welcome to the Dashboard!</h1>");
+      });
       cy.visit("/");
       // cy.login();
     });
@@ -9,12 +45,12 @@ describe("About", () => {
       cy.contains("h1", "Welcome to ProductStory");
     });
 
-    it("has a Sign In button", () => {
-      cy.contains("button.sign-in-button", "Sign In");
-    });
-
-    it("has a headline in the toolbar", () => {
-      cy.contains("div.v-toolbar__title.headline", "ProductStory");
-    });
+    // it("has a Sign In button", () => {
+    //   cy.contains("button.sign-in-button", "Sign In");
+    // });
+    //
+    // it("has a headline in the toolbar", () => {
+    //   cy.contains("div.v-toolbar__title.headline", "ProductStory");
+    // });
   });
 });
